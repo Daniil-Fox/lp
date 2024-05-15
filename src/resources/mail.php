ca5d85bce9a3e051b63a4dd7c005b933c5f7ddc6
Artyushenko_Daniil <foxones80@gmail.com>
2024-05-15 23:12:13 +0600

    fixed styles

--- src/resources/mail.php
+++ src/resources/mail.php
@@ -32,16 +32,16 @@ try {
   $mail->SMTPAuth   = true;
 
   // Настройки вашей почты
-  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-  $mail->Username   = ''; // Логин на почте
-  $mail->Password   = ''; // Пароль на почте
+  $mail->Host       = 'smtp.gmail.ru'; // SMTP сервера вашей почты
+  $mail->Username   = 'foxones80@gmail.com'; // Логин на почте
+  $mail->Password   = 'artyomov1ch'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
   $mail->setFrom('', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
 
   // Получатель письма
-  $mail->addAddress('');
+  $mail->addAddress('foxones80@gmail.com');
 
   // Прикрипление файлов к письму
   if (!empty($file['name'][0])) {
